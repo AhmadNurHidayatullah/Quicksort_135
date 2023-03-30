84f3775f79dad275457b3c343f595d093a10b39f
AhmadNurHidayatullah <127074146+AhmadNurHidayatullah@users.noreply.github.com>
2023-03-30 15:43:38 +0700

    void q_short (low,high)

--- Quicksort_135/Quicksort_135.cpp
+++ Quicksort_135/Quicksort_135.cpp
@@ -80,7 +80,19 @@ void q_short(int low, int high)
             mov_count++;
         }
     }
-    
+    // j now countaint the index of the last element in the sorted list 
+    if (low < j)                                // Langkah algoritma no.11
+    {
+        //move the pivot to its correct position in the list
+        swap(low, j);
+        mov_count++;
+    }
+    //sort the list on the left of pivot using quick sort
+    q_short(low, j - 1);                        // Langkah algoritma no. 12
+
+    //sort the list on the right of pivot using quick sort
+    q_short(j + 1, high);                             // Langkah Algoritma no.13
+
 }
 int main()
 {
